6a2f5625428deea8894a9b3ef9e6b2f9d8f4a8d7
vipulkunwar000 <95694307+vipulkunwar000@users.noreply.github.com>
2021-12-28 09:14:53 +0530

    Add files via upload

---
+++ weather2.py
@@ -0,0 +1,16 @@
+from urllib.request import urlopen
+def main():
+    API_KEY = "6816f97c850698ada526c130afa5674f"
+    city = input('Enter a city name: ')
+
+    # open a connection to a URL using urllib2
+    web_url = urlopen("https://api.openweathermap.org/data/2.5/weather?appid="+API_KEY+"&q="+city)
+    # Get a result code and print it
+    print("Result code:"+str(web_url.getcode()))
+    # Read the data from url and print it
+    data = web_url.read()
+    print(data)
+
+    
+if __name__=="__main__":
+    main()
